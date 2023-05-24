721545a20b5a3bcaf16eb65fa9479402e67c7bb8
mayoreee <mayoreee1@gmail.com>
2023-05-24 23:32:33 +0200

    feat: remove jmes-testnet-1

--- src/config.js
+++ src/config.js
@@ -1,9 +1,5 @@
 export const networks = [
   {
-    chainId: 'jmes-tesnet-1',
-    faucetUrl: 'https://faucet-api-uopuy.ondigitalocean.app/credit',
-  },
-  {
     chainId: 'jmes-tesnet-3',
     faucetUrl: 'https://faucet-api-uopuy.ondigitalocean.app/credit',
   },
